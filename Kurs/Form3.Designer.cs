f277ac919899604e24219ce5ddb34c12f056a652
Cy_Nec <cy.nec01@gmail.com>
2025-05-18 16:48:59 +0500

    Функционал интерфейса для окна с Латинским квадратом

--- Kurs/Form3.Designer.cs
+++ Kurs/Form3.Designer.cs
@@ -33,18 +33,30 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBoxBasic = new System.Windows.Forms.ListBox();
             this.buttonBack = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.buttonGen = new System.Windows.Forms.Button();
+            this.listBoxExtra = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(135, 34);
+            this.label1.Location = new System.Drawing.Point(135, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(138, 16);
             this.label1.TabIndex = 0;
@@ -70,28 +82,32 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(610, 9);
+            this.textBox1.Location = new System.Drawing.Point(603, 9);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(22, 20);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(29, 20);
             this.textBox1.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(427, 70);
+            this.label3.Location = new System.Drawing.Point(427, 62);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(190, 16);
             this.label3.TabIndex = 4;
             this.label3.Text = "Выберите основной фактор";
             // 
-            // listBox1
+            // listBoxBasic
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(430, 115);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(187, 95);
-            this.listBox1.TabIndex = 5;
+            this.listBoxBasic.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.listBoxBasic.FormattingEnabled = true;
+            this.listBoxBasic.ItemHeight = 17;
+            this.listBoxBasic.Location = new System.Drawing.Point(430, 81);
+            this.listBoxBasic.Name = "listBoxBasic";
+            this.listBoxBasic.Size = new System.Drawing.Size(187, 89);
+            this.listBoxBasic.TabIndex = 5;
+            this.listBoxBasic.SelectedIndexChanged += new System.EventHandler(this.listBoxBasic_SelectedIndexChanged);
             // 
             // buttonBack
             // 
@@ -115,12 +131,91 @@
             // 
             // buttonGen
             // 
-            this.buttonGen.Location = new System.Drawing.Point(473, 250);
+            this.buttonGen.Location = new System.Drawing.Point(473, 336);
             this.buttonGen.Name = "buttonGen";
             this.buttonGen.Size = new System.Drawing.Size(91, 23);
             this.buttonGen.TabIndex = 8;
             this.buttonGen.Text = "Генерация";
             this.buttonGen.UseVisualStyleBackColor = true;
+            this.buttonGen.Click += new System.EventHandler(this.buttonGen_Click);
+            // 
+            // listBoxExtra
+            // 
+            this.listBoxExtra.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.listBoxExtra.FormattingEnabled = true;
+            this.listBoxExtra.ItemHeight = 17;
+            this.listBoxExtra.Location = new System.Drawing.Point(430, 214);
+            this.listBoxExtra.Name = "listBoxExtra";
+            this.listBoxExtra.Size = new System.Drawing.Size(187, 89);
+            this.listBoxExtra.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(427, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(199, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Выберите вторичный фактор";
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.buttonRight.Location = new System.Drawing.Point(405, 335);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(23, 23);
+            this.buttonRight.TabIndex = 11;
+            this.buttonRight.Text = ">";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.buttonLeft.Location = new System.Drawing.Point(384, 335);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(24, 23);
+            this.buttonLeft.TabIndex = 12;
+            this.buttonLeft.Text = "<";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(35, 62);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(343, 297);
+            this.dataGridView2.TabIndex = 13;
+            this.dataGridView2.Visible = false;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(35, 62);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(343, 297);
+            this.dataGridView3.TabIndex = 14;
+            this.dataGridView3.Visible = false;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(35, 62);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(343, 297);
+            this.dataGridView4.TabIndex = 15;
+            this.dataGridView4.Visible = false;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(35, 62);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.Size = new System.Drawing.Size(343, 297);
+            this.dataGridView5.TabIndex = 16;
+            this.dataGridView5.Visible = false;
             // 
             // FormLat
             // 
@@ -128,10 +223,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(644, 418);
+            this.Controls.Add(this.dataGridView5);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.listBoxExtra);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.buttonGen);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.buttonBack);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBoxBasic);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -140,6 +243,10 @@
             this.Name = "FormLat";
             this.Text = "Латинский квадрат";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,9 +259,17 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBoxBasic;
         private System.Windows.Forms.Button buttonBack;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button buttonGen;
+        private System.Windows.Forms.ListBox listBoxExtra;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridView dataGridView5;
     }
 }
--- Kurs/Form3.cs
+++ Kurs/Form3.cs
@@ -4,24 +4,135 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Kurs
 {
     public partial class FormLat : Form
     {
+        private int indexDataGrid = 1;
         private int[] dataCollection;
+        private string[] factorNames = { "A", "B", "C", "D", "E" };
         public FormLat(int[] collectionData)
         {
             InitializeComponent();
             this.dataCollection = collectionData;
+
+            for (int i = 0; i < dataCollection.Length; i++)
+            {
+                string item = $"{factorNames[i]}: {dataCollection[i]}";
+                listBoxBasic.Items.Add(item);
+            }
         }
 
         private void buttonBack_Click(object sender, EventArgs e)
         {
             FormBase formBase = new FormBase();
             this.Close();
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            indexDataGrid += 1;
+            if (indexDataGrid > dataCollection.Length-1) indexDataGrid = 1;
+            switch (indexDataGrid)
+            {
+                case 1:
+                    dataGridView1.Visible = true;
+                    dataGridView5.Visible = false;
+                    label1.Text = $"План эксперимента №{indexDataGrid}";
+                    break;
+                case 2:
+                    dataGridView1.Visible = false;
+                    dataGridView2.Visible = true;
+                    label1.Text = $"План эксперимента №{indexDataGrid}";
+                    break;
+                case 3:
+                    dataGridView3.Visible = true;
+                    dataGridView2.Visible = false;
+                    label1.Text = $"План эксперимента №{indexDataGrid}";
+                    break;
+                case 4:
+                    dataGridView4.Visible = true;
+                    dataGridView3.Visible = false;
+                    label1.Text = $"План эксперимента №{indexDataGrid}";
+                    break;
+                case 5:
+                    dataGridView5.Visible = true;
+                    dataGridView4.Visible = false;
+                    label1.Text = $"План эксперимента №{indexDataGrid}";
+                    break;
+            }
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            indexDataGrid -= 1;
+            if (indexDataGrid < 1) indexDataGrid = dataCollection.Length - 1;
+            switch (indexDataGrid)
+            {
+                case 1:
+                    dataGridView1.Visible = true;
+                    dataGridView2.Visible = false;
+                    label1.Text = $"План эксперимента №{indexDataGrid}";
+                    break;
+                case 2:
+                    dataGridView3.Visible = false;
+                    dataGridView2.Visible = true;
+                    label1.Text = $"План эксперимента №{indexDataGrid}";
+                    break;
+                case 3:
+                    dataGridView3.Visible = true;
+                    dataGridView4.Visible = false;
+                    label1.Text = $"План эксперимента №{indexDataGrid}";
+                    break;
+                case 4:
+                    dataGridView4.Visible = true;
+                    dataGridView5.Visible = false;
+                    label1.Text = $"План эксперимента №{indexDataGrid}";
+                    break;
+                case 5:
+                    dataGridView5.Visible = true;
+                    dataGridView1.Visible = false;
+                    label1.Text = $"План эксперимента №{indexDataGrid}";
+                    break;
+            }
+        }
+
+        private void buttonGen_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Метод для получения соответствующего DataGridView по индексу
+        private DataGridView GetDataGridView(int index)
+        {
+            switch (index)
+            {
+                case 0: return dataGridView1;
+                case 1: return dataGridView2;
+                case 2: return dataGridView3;
+                case 3: return dataGridView4;
+                case 4: return dataGridView5;
+                default: throw new ArgumentException("Недопустимый индекс DataGridView.");
+            }
+        }
+
+        private void ClearDataGridViews()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            dataGridView3.Rows.Clear();
+            dataGridView3.Columns.Clear();
+            dataGridView4.Rows.Clear();
+            dataGridView4.Columns.Clear();
+            dataGridView5.Rows.Clear();
+            dataGridView5.Columns.Clear();
         }
     }
 }
